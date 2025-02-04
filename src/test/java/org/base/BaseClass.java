307143739c77911b94e6f3e9b268e808e367486d
krits143 <akil.kirthi@gmail.com>
2025-02-04 10:54:15 +0530

    changed code

--- src/test/java/org/base/BaseClass.java
+++ src/test/java/org/base/BaseClass.java
@@ -3,7 +3,7 @@ package org.base;
 public class BaseClass {
 	
 	public static void main(String[] args) {
-		System.out.println("Hi");
+		System.out.println("task1 commk");
 		System.out.println("Hello World");
 		System.out.println("Task Completed");
 	}
